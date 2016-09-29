327c936a4845ae0b54f160f2fc1eefadd39b0544
name <freiny50@gmail.com>
2016-09-29 00:01:32 -0500

    Add lang/variables/masking

---
+++ lang/variables/mutable.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var i = 10;
+console.log(i);
+// OUTPUT: 10
+
+i = 'hello';
+console.log(i);
+// OUTPUT: hello
+
+i = {'key': 'value'};
+console.log(i);
+// OUTPUT: { key: 'value' }
+
+i = 4.7;
+console.log(i);
+// OUTPUT: 4.7
